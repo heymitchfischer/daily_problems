f3cd35879632c8c899546cb9e37af39830a38127
heymitchfischer <mitchfischer6@gmail.com>
2017-08-22 21:27:09 -0500

    added sunday.rb and monday.rb

--- week_1/monday.rb
+++ week_1/monday.rb
@@ -1,1 +1,8 @@
-# Monday
+items = [
+  {id: 1, body: 'foo'},
+  {id: 2, body: 'bar'},
+  {id: 3, body: 'foobar'}
+]
+
+items = items.map{|x| x[id:]}
+p items
---
+++ week_1/sunday.rb
@@ -0,0 +1,8 @@
+numbers = [1, 2, 4, 2]
+# doubled_numbers = []
+# numbers.each do |number|
+#   doubled_numbers << number * 2
+# end
+# p doubled_numbers
+numbers = numbers.map{|x| x * 2}
+p numbers

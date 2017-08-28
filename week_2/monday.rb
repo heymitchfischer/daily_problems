c13e48460098ae3187b410d94c45fc24402001e2
heymitchfischer <mitchfischer6@gmail.com>
2017-08-28 17:58:10 -0500

    added week_2

---
+++ week_2/monday.rb
@@ -0,0 +1,9 @@
+movies = [
+  {id: 1, title: "Die Hard", rating: 4.0},
+  {id: 2, title: "Bad Boys", rating: 5.0},
+  {id: 3, title: "The Chamber", rating: 3.0},
+  {id: 4, title: "Fracture", rating: 2.0}
+]
+
+new_movies = movies.select{|movie| movie[:rating] < 4.0}
+p new_movies
---
+++ week_2/sunday.rb
@@ -0,0 +1,9 @@
+numbers = [1, 2, 4, 2]
+even_numbers = numbers.select{|num| num.even?}
+# even_numbers = []
+# numbers.each do |number|
+#   if number.even?
+#     even_numbers << number
+#   end
+# end
+p even_numbers
